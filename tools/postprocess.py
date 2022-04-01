a508684fdbb2245659345d4da0d0517983b8314e
Vuillemot Florian <florian.vuillemot@posteo.net>
2022-04-01 11:56:58 +0200

    Post-process cross-references in tables (#76)
    Co-authored-by: Fabien Lelaquais <86590727+FabienLelaquais@users.noreply.github.com>

--- tools/postprocess.py
+++ tools/postprocess.py
@@ -234,6 +234,35 @@ def on_post_build(env):
                     if last_location:
                         file_was_changed = True
                         html_content = new_content + html_content[last_location:]
+
+                    # Finding xrefs in 'Parameters', 'Returns' and similar constructs.
+                    # These would be <typeName>^ fragments located in potentially
+                    # complex constructs such as "Optional[Union[str, <typeName>^]]"
+                    #
+                    # These fragments appear in single-line <td> blocks.
+                    #
+                    # At this point, this code is pretty sub-optimal and heavily
+                    # depends on the MkDocs generation. Time will tell if we can
+                    # keep it as is...
+                    typing_code = re.compile(r"(<td><code>)(.*\^.*)(</code></td>)")
+                    # This will match a single <typeName>^ fragment.
+                    typing_type = re.compile(r"\w+\^")
+                    for xref in typing_code.finditer(html_content):
+                        groups = xref.groups()
+                        table_line = groups[1]
+                        table_line_to_replace = "".join(groups)
+                        new_table_line = table_line_to_replace
+
+                        for type_ in typing_type.finditer(table_line):
+                            class_ = type_[0][:-1] # Remove ^
+                            packages = xrefs.get(class_)
+                            if packages:
+                                new_content = f"<a href='{rel_path}/{package}.{class_}'>{class_}</a>"
+                                new_table_line = new_table_line.replace(f"{class_}^", new_content)
+                                file_was_changed = True
+                        if file_was_changed:
+                            html_content = html_content.replace(table_line_to_replace, new_table_line)
+
                 if file_was_changed:
                     with open(filename, "w") as html_file:
                         html_file.write(html_content)
