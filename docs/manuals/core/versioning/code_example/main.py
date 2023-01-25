f097d3c9f820a33a6191edbf92f99df384818ecd
Jean-Robin <jeanrobin.medori@avaiga.com>
2023-01-25 20:01:22 +0100

    Update docs/manuals/core/versioning/code_example/main.py
    Co-authored-by: Đỗ Trường Giang <do.giang@avaiga.com>

--- docs/manuals/core/versioning/code_example/main.py
+++ docs/manuals/core/versioning/code_example/main.py
@@ -7,7 +7,7 @@ def my_print_algo(entry: str):
     return entry
 
 
-input_cfg = Config.configure_data_node("input", default_data="a_sting")
+input_cfg = Config.configure_data_node("input", default_data="a_string")
 output_cfg = Config.configure_data_node("output")
 task_cfg = Config.configure_task("my_print_algo", my_print_algo, input_cfg, output_cfg)
 scenario_cfg = Config.configure_scenario_from_tasks("my_scenario", [task_cfg])
