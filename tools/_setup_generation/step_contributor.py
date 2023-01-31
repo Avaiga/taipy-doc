680a7ed1677861392341e0045ce36f172504ec24
jrobinAV <88036007+jrobinAV@users.noreply.github.com>
2023-01-31 18:07:29 +0100

    First version of automated credits page

--- tools/_setup_generation/step_contributor.py
+++ tools/_setup_generation/step_contributor.py
@@ -5,7 +5,7 @@
 # retrieved using GitHub REST APIs.
 # ################################################################################
 import os
-
+import random
 import requests
 
 from .setup import SetupStep, Setup
@@ -15,24 +15,19 @@ class ContributorStep(SetupStep):
 
     def __init__(self):
         self.BASE_URL = "https://api.github.com"
-        self.ORGANIZATION_URL = f"{self.BASE_URL}/orgs/avaiga/members"
-        self.REPOSITORIES = [
-            "taipy-config",
-            "taipy-core",
-            "taipy-gui",
-            "taipy-rest",
-            # "taipy-auth",
-            # "taipy-enterprise",
-        ]
-        self.REPO_URLS = list(map(lambda _: f"{self.BASE_URL}/repos/avaiga/" + _ + "/contributors", self.REPOSITORIES))
-        self.PATH = ""
+        self.ORGANIZATION_URL = f"{self.BASE_URL}/orgs/avaiga"
+        self.MEMBERS_URL = f"{self.ORGANIZATION_URL}/members"
+        self.REPOS = f"{self.ORGANIZATION_URL}/repos"
+        self.REPO_URLS = []
+        self.MEMBERS = {}
+        self.CONTRIBUTORS = {}
         self.ANONYMOUS = ["dependabot[bot]"]
-        self.content = """# Contributors
+        self.PATH = ""
+        self.TEMPLATE_SUFFIX = "_template"
 
-A special thanks go to our contributors:\n"""
 
     def enter(self, setup: Setup):
-        self.PATH = os.path.join(setup.docs_dir, "credits", "contributors.md")
+        self.PATH = os.path.join(setup.docs_dir, "credits", "development_lead.md")
 
     def get_id(self) -> str:
         return "contributors"
@@ -41,37 +36,62 @@ A special thanks go to our contributors:\n"""
         return "Generating the contributors"
 
     def setup(self, setup: Setup) -> None:
-        avaiga_members = self.get_avaiga_members()
-        contributors = self.get_contributors(avaiga_members)
-        self.build_content(contributors)
+        self.get_repo_urls()
+        self.get_avaiga_members()
+        self.get_contributors()
+        self.build_content((self.MEMBERS, "[AVAIGA_TEAM_MEMBERS]"), (self.CONTRIBUTORS, "[TAIPY_CONTRIBUTORS]"))
+
+    def get_repo_urls(self):
+        repos = requests.get(self.REPOS).json()
+        self.REPO_URLS = list(map(lambda _: _['url'], repos))
 
     def get_avaiga_members(self):
-        members = requests.get(self.ORGANIZATION_URL).json()
-        return list(map(lambda _: _['login'], members))
+        members = requests.get(self.MEMBERS_URL).json()
+        for c in members:
+            login = c['login']
+            if login not in self.MEMBERS and login not in self.ANONYMOUS:
+                self.MEMBERS[login] = {"avatar_url": c['avatar_url'], "html_url": c['html_url']}
 
-    def get_contributors(self, avaiga_members):
-        contributors = {}
+    def get_contributors(self):
         for url in self.REPO_URLS:
-            response = requests.get(url)
+            response = requests.get(url+"/contributors")
             for c in response.json():
-                # print(c)
                 login = c['login']
-                contributors[login] = {"avatar_url": c['avatar_url'],
-                                       "html_url": c['html_url'],
-                                       "avaiga_member": True if login in avaiga_members else False}
-        return contributors
+                if login not in self.MEMBERS and login not in self.ANONYMOUS:
+                    self.CONTRIBUTORS[login] = {"avatar_url": c['avatar_url'], "html_url": c['html_url']}
 
-    def build_content(self, contributors):
-        contrib_list = list(contributors.items())
-        import random
-        random.shuffle(contrib_list)
-        for login, contributor_info in contrib_list:
-            if login not in self.ANONYMOUS:
-                self.content += f"\n- [<img src='{contributor_info['avatar_url']}' alt='avatar' width='20'/>" \
-                                f" {login}]" \
-                                f"({contributor_info['html_url']})"
-        self.content += "\n"
+    def build_content(self, *members_pattern_tuples):
+        pattern_content_tuples = []
+        for member_pattern in members_pattern_tuples:
+            members = member_pattern[0]
+            pattern = member_pattern[1]
+            content = ""
+            members_list = list(members.items())
+            random.shuffle(members_list)
+            for login, member_info in members_list:
+                if login not in self.ANONYMOUS:
+                    content += f"\n- [<img src='{member_info['avatar_url']}' alt='avatar' width='20'/>" \
+                                    f" {login}]" \
+                                    f"({member_info['html_url']})"
+            content += "\n"
+            pattern_content_tuples.append((pattern, content))
+
+        self._replace(self.PATH, *pattern_content_tuples)
+
+    def _replace(self, path, *pattern_content_tuples):
+        # Read in the file
+        with open(path + self.TEMPLATE_SUFFIX, 'r') as file:
+            file_data = file.read()
+
+        # Replace the patterns by the contents
+        for pattern_content in pattern_content_tuples:
+            pattern = pattern_content[0]
+            content = pattern_content[1]
+            file_data = file_data.replace(pattern, content)
+
+        # Write the file out without the template suffix
+        with open(path, 'w') as file:
+            file.write(file_data)
 
     def exit(self, setup: Setup):
-        with open(self.PATH, "w") as f:
-            f.write(self.content)
+        pass
