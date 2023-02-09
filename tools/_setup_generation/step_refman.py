822f4e98854b0621f893eb39d5b17f8fe9dce6a1
jrobinAV <88036007+jrobinAV@users.noreply.github.com>
2023-02-09 21:24:40 +0100

    Fix aliases

--- tools/_setup_generation/step_refman.py
+++ tools/_setup_generation/step_refman.py
@@ -171,8 +171,14 @@ class RefManStep(SetupStep):
                     if e.__module__.startswith(Setup.ROOT_PACKAGE):
                         if e.__class__.__name__ == "NewType":
                             entry_type = TYPE_ID
+                    elif e.__module__ == "typing" and hasattr(e, "__name__"):
+                        # Manually remove class from 'typing'
+                        if e.__name__ == "NewType":
+                            continue
+                        if e.__name__ == "overload":
+                            continue
+                        entry_type = TYPE_ID
                     else:
-                        # Not in our focus package?
                         continue
                 # Remove hidden entries
                 if entry in RefManStep.HIDDEN_ENTRIES:
