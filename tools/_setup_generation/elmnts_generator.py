1cc40303f7f74709a2bf786e79a2d10e453ed3a1
Fabien Lelaquais <fabien@lelaquais.fr>
2023-06-08 21:51:55 +0200

    Fix anchor to indexed properties in visual elements

--- tools/_setup_generation/elmnts_generator.py
+++ tools/_setup_generation/elmnts_generator.py
@@ -155,7 +155,7 @@ class ElementsGenerator(SetupStep):
                 doc  = property.get("doc", None)
                 if not default_value:
                     default_value = "<i>Required</i>" if property.get("required", False) else ""
-                full_name = f"<code id=\"p-{name}\">"
+                full_name = f"<code id=\"p-{re.sub('<[^>]+>', '', name)}\">"
                 if name == default_property_name:
                     full_name += f"<u><bold>{name}</bold></u></code><sup><a href=\"#dv\">{STAR}</a></sup>"
                 else:
