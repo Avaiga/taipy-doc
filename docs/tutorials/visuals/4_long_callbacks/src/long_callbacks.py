b5dd2d93f78befa520508484d5ce7190d1dc6502
Alexandre Sajus <alexandre.sajus@student-cs.fr>
2024-03-21 11:50:31 +0100

    Improves the long callback example for clarity

--- docs/tutorials/visuals/4_long_callbacks/src/long_callbacks.py
+++ docs/tutorials/visuals/4_long_callbacks/src/long_callbacks.py
@@ -1,62 +1,86 @@
-from taipy.gui import Gui, Markdown, invoke_long_callback, notify
-import numpy as np
+from taipy.gui import Gui, Markdown, invoke_long_callback, notify, State
 
 
 status = 0
-num_iterations = 10_000_000
+num_iterations = 20_000_000
 pi_list = []
+logs = "Not running"
 
 
-def pi_approx(num_iterations):
+def pi_approx(num_iterations: int):
+    """
+    Approximate Pi using the Leibniz formula.
+
+    Args:
+        num_iterations: Number of iterations to compute the approximation.
+
+    Returns:
+        A list of approximations of Pi, made at each iteration.
+    """
     k, s = 3.0, 1.0
     pi_list = []
     for i in range(num_iterations):
-        s = s-((1/k) * (-1)**i)
+        s = s - ((1 / k) * (-1) ** i)
         k += 2
-        if (i+1)%(int(num_iterations/1_000)+1) == 0:
-            pi_list += [np.abs(4*s-np.pi)]
+        if (i + 1) % (int(num_iterations / 100) + 1) == 0:
+            pi_list += [4 * s]
 
     return pi_list
 
-    
-def heavy_status(state, status, pi_list):
-    notify(state, 'i', f"Status parameter: {status}")
+
+def heavy_status(state: State, status, pi_list: list):
+    """
+    Periodically update the status of the long callback.
+
+    Args:
+        state: The state of the application.
+        status: The status of the long callback.
+        pi_list: The list of approximations of Pi.
+    """
+    state.logs = f"Approximating Pi... ({status}s)"
     if isinstance(status, bool):
         if status:
-            notify(state, 'success', "Finished")
+            state.logs = f"Finished! Approximation: {pi_list[-1]}"
+            notify(state, "success", "Finished")
             state.pi_list = pi_list
         else:
-            notify(state, 'error', f"An error was raised")
+            notify(state, "error", f"An error was raised")
     else:
         state.status += 1
-    
-def on_action(state):
-    invoke_long_callback(state,
-                         pi_approx, [int(state.num_iterations)],
-                         heavy_status, [],
-                         2000)  
 
-       
-page = Markdown("""
-How many times was the status function called? <|{status}|>
 
-## Number of approximation
+def on_action(state: State):
+    """
+    When the button is clicked, start the long callback.
 
-<|{num_iterations}|number|label=# of approximation|>
+    Args:
+        state: The state of the application.
+    """
+    invoke_long_callback(
+        state, pi_approx, [int(state.num_iterations)], heavy_status, [], 1000
+    )
 
-<|Approximate pie|button|on_action=on_action|>
 
+page = Markdown(
+    """
+# Approximating **Pi**{: .color-primary} using the Leibniz formula
+<|{num_iterations}|number|label=Number of iterations|>
+<|Approximate Pi|button|on_action=on_action|>
 ## Evolution of approximation
 <|{pi_list}|chart|layout={layout}|>
-""")
+<br/>
+<|card|
+## Logs
+### <|{logs}|text|raw|>
+|>	
+"""
+)
 
 
 layout = {
-    "yaxis": {
-    "type": 'log',
-    "autorange": True
-    }
-  }
+    "xaxis": {"title": "Iteration (Percentage of Total Iterations)"},
+    "yaxis": {"title": "Pi Approximation"},
+}
 
 
 Gui(page).run()
